fedb02583e48affffa4ef82bc5670beb8ac9f7e1
alexeybokov <lexabokov18@gmail.com>
2019-02-12 18:50:01 +0200

    add routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'dashboard#index'
+
+  resources :books do
+    post  :like,    on: :member
+    put   :take,    on: :member
+    put   :return,  on: :member
+  end
 end
